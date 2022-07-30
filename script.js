674dc1d19bd31d61d990770558ac6942fbe02f00
Chernandez2022 <hcarlos0816@gmail.com>
2022-07-30 18:41:28 -0400

    added button

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-let questions = [
-  {
-    prompt:
-      "What is the capital of New Jersey? \n (a) Wayne \n (b) Paramus \n (c) Trenton \n (d) (Paterson)",
-    answer: "c",
-  },
-  {
-    prompt:
-      "Where can Kangaroos be found? \n (a) Australia \n (b) Mexico \n (c) Bolivia \n (d) Japan",
-    answer: "a",
-  },
-  {
-    prompt: "What's 9 + 10? \n (a) 21 \n (b) 11 \n (c) 20 \n (d) 19",
-    answer: "d",
-  },
-  {
-    prompt:
-      "How many months have 28 days?? \n (a) 6\n (b) 12 \n (c) 3 \n (d) 5",
-    answer: "b",
-  },
-  {
-    prompt:
-      "What are two things you can never eat for breakfast? \n (a) Lunch and Dinner\n (b) Candy and more candy \n (c) Steak and milk \n (d) Cookies and Milk",
-    answer: "a",
-  },
-  {
-    prompt:
-      "What never asks a question but gets answered all the time?? \n a Computer\n (b) Cellphone \n (c) The door \n (d) A delivery",
-    answer: "b",
-  },
-  {
-    prompt:
-      "What do you call a woman who knows where her husband is all the time? \n (a) Karen\n (b) Overbearing \n (c) A widow \n (d) Paranoid",
-    answer: "c",
-  },
-  {
-    prompt:
-      "What goes up and down, but always remains in the same place? \n (a) Elevator\n (b) Plane \n (c) Age \n (d) Stairs",
-    answer: "d",
-  },
-];
+// let questions = [
+//   {
+//     prompt:
+//       "What is the capital of New Jersey? \n (a) Wayne \n (b) Paramus \n (c) Trenton \n (d) (Paterson)",
+//     answer: "c",
+//   },
+//   {
+//     prompt:
+//       "Where can Kangaroos be found? \n (a) Australia \n (b) Mexico \n (c) Bolivia \n (d) Japan",
+//     answer: "a",
+//   },
+//   {
+//     prompt: "What's 9 + 10? \n (a) 21 \n (b) 11 \n (c) 20 \n (d) 19",
+//     answer: "d",
+//   },
+//   {
+//     prompt:
+//       "How many months have 28 days?? \n (a) 6\n (b) 12 \n (c) 3 \n (d) 5",
+//     answer: "b",
+//   },
+//   {
+//     prompt:
+//       "What are two things you can never eat for breakfast? \n (a) Lunch and Dinner\n (b) Candy and more candy \n (c) Steak and milk \n (d) Cookies and Milk",
+//     answer: "a",
+//   },
+//   {
+//     prompt:
+//       "What never asks a question but gets answered all the time?? \n a Computer\n (b) Cellphone \n (c) The door \n (d) A delivery",
+//     answer: "b",
+//   },
+//   {
+//     prompt:
+//       "What do you call a woman who knows where her husband is all the time? \n (a) Karen\n (b) Overbearing \n (c) A widow \n (d) Paranoid",
+//     answer: "c",
+//   },
+//   {
+//     prompt:
+//       "What goes up and down, but always remains in the same place? \n (a) Elevator\n (b) Plane \n (c) Age \n (d) Stairs",
+//     answer: "d",
+//   },
+// ];
 
-let counter = 0;
-for (let i = 0; i < questions.length; i++) {
-  let response = window.prompt(questions[i].prompt);
-  if (response == questions[i].answer) {
-    counter++;
-    alert("Correct!!!");
-  } else {
-    alert("Wrong!!!");
-  }
-}
+// let counter = 0;
+// for (let i = 0; i < questions.length; i++) {
+//   let response = window.prompt(questions[i].prompt);
+//   if (response == questions[i].answer) {
+//     counter++;
+//     alert("Correct!!!");
+//   } else {
+//     alert("Wrong!!!");
+//   }
+// }
 
-alert("You got" + counter + "correct");
+// alert("You got" + counter + "correct");
